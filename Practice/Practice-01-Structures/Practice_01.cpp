e4d83ed3501857e7b6d8f032a9aae9f9c126181d
VasiPeycheva <vasilena.peicheva@gmail.com>
2018-03-01 15:08:39 +0200

    Repo reorganization

---
+++ Practice/Practice-01-Structures/Practice_01.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+
+// 1. Създаваме необходимата структура
+// 2. Как можем да достъпим членове, декларация
+// 3. Инициализация
+// 4. Извеждане на данни
+// 5. Заделяне на динамична структура.
+// 6. Въвеждане на данни, почистване на паметта.
+
+
+struct Point 
+{
+	int x;
+	int y;
+};
+
+void initializePoint(Point & point)
+{
+	std::cin >> point.x;
+	std::cin >> point.y;
+}
+
+void printPoint(const Point & point)
+{
+	std::cout << "(" << point.x << ","
+			  << point.y << ")" << std::endl;
+}
+
+int delta(int X0, int X1)
+{
+	return X1 - X0;
+}
+
+double distance(const Point & A, const Point & B)
+{
+	// get the difference between each coordinate
+
+	int deltaX = delta(A.x, B.x);
+	int deltaY = delta(A.y, B.y);
+	
+	double result = sqrt(pow(deltaX, 2) + pow(deltaY, 2));
+
+	return result;
+}
+
+
+void createPointArr(Point *& arr, int size)
+{
+	arr = new(std::nothrow) Point[size];
+	if (!arr)
+	{
+		std::cerr << "Not enough memory to complete the task!" << std::endl;
+		return;
+	}
+}
+
+void initializePointArr(Point * arr,int size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		initializePoint(arr[i]);
+	}
+}
+
+void printPointArr(Point * arr, int size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		std::cout << "point p" << i << " coordinates: ";
+		printPoint(arr[i]);
+	}
+}
+
+
+int main()
+{
+	Point* arr;
+	createPointArr(arr, 2);
+	initializePointArr(arr, 2);
+	printPointArr(arr, 2);
+
+	std::cout << "The distance between the points is: " << distance(arr[0], arr[1]) << std::endl;
+
+	return 0;
+}
